62f591a0075d6a0d40b9e51292772822b520db0e
Carlos Smartdevices <carlos@smartdevices.com>
2025-12-26 05:08:32 +0000

    mmm

--- src/bot/api/routes.js
+++ src/bot/api/routes.js
@@ -11,6 +11,7 @@ const logger = require('../../utils/logger');
 const webhookController = require('./controllers/webhookController');
 const subscriptionController = require('./controllers/subscriptionController');
 const paymentController = require('./controllers/paymentController');
+const invitationController = require('./controllers/invitationController');
 
 // Middleware
 const { asyncHandler } = require('./middleware/errorHandler');
